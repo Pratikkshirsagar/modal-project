af1ae4d0558abd0210acf0fc5b4b9b574dcc3012
Pratik Kshirsagar <meetpratiik1995@gmail.com>
2021-05-17 00:35:28 +0530

    prop pass

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <h1>{{ title }}</h1>
+  <Modal :header="header" :text="text" theme="sale" />
 </template>
 
 <script>
+import Modal from './components/Modal'
 
 export default {
   name: 'App',
+  components: { Modal },
   data() {
     return {
-      title: 'My First Vue App :)'
+      title: 'My First Vue App :)',
+      header: 'sign up for the Giveaway',
+      text: 'Grab your ninja swag for half price'
     }
-    
   }
 }
 </script>
